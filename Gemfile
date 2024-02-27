b3a923796d0329a5a81ce0cec8d5900599066ea6
cornet <cornet@sheepy.org>
2024-02-27 17:12:19 +0000

    Add some basic rspec tests
    This script is in need of refactoring as it is hard to follow the logic
    making it difficult to make changes to.
    
    This adds some very basic tests to test the following so we can begin
    refactoring:
    
    - Test when deploy successful
    - Test failure due to Sidekiq not starting
    - Test failure due to Forward deploy check failure
    
    All other errors will result in a fairly generic error message and the
    above should be enough to enable us to test the refactor.
    
    The aim is once we have refactored we can add a better test suite.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org' do
   gem 'aws-sdk-states'
+
+  gem 'nokogiri'
+  gem 'rspec'
+  gem 'rspec-mocks'
 end
---
+++ spec/app_spec.rb
@@ -0,0 +1,117 @@
+require 'rspec'
+require 'stringio'
+
+require 'aws-sdk-states'
+
+RSpec.describe 'app' do
+  let(:mock_client) { double('Aws::States::Client') }
+  let(:execution_arn) { 'arn:aws:states:eu-west-1:123456789012:execution:my-execution-flow' }
+  let(:test_github_log) { Tempfile.new('github_output') }
+
+  before(:each) do
+    ENV['EXECUTION_ARN'] = execution_arn
+    ENV['GITHUB_OUTPUT'] = test_github_log.path
+    allow(Aws::States::Client).to receive(:new).and_return(mock_client)
+    allow(mock_client).to receive(:describe_execution).with(any_args).and_return(desc_exec_resp)
+    allow(mock_client).to receive(:get_execution_history).with(any_args).and_return(get_exec_history)
+  end
+
+  context 'when deployment was successful' do
+    let(:desc_exec_resp) { Aws::States::Types::DescribeExecutionOutput.new(status: 'SUCCEEDED') }
+    let(:get_exec_history) {}
+
+    it 'outputs "Deployment Successful 🎉"' do
+      $stdout = StringIO.new
+
+      # Execute the script
+      load "#{__dir__}/../app.rb"
+
+      # Assert the output
+      expect($stdout.string).to eq("Deployment Successful 🎉\n")
+    end
+  end
+
+  context 'when forward deploy check failed' do
+    let(:desc_exec_resp) { Aws::States::Types::DescribeExecutionOutput.new(status: 'FAILED') }
+    let(:get_exec_history) do
+      Aws::States::Types::GetExecutionHistoryOutput.new(
+        events: [
+          '',
+          Aws::States::Types::HistoryEvent.new(
+            type: 'LambdaFunctionFailed',
+            lambda_function_failed_event_details: Aws::States::Types::LambdaFunctionFailedEventDetails.new(
+              cause: '
+                {
+                  "errorMessage": "Pre flight checks failed:\n{\"Checks\"=>{:RequiredParameters=>\"PASSED\", :CommitCheck=>\"PASSED\", :ScheduleCheck=>\"PASSED\", :ForwardDeployCheck=>\"FAILED\"}, \"Status\"=>\"FAILED\"}",
+                  "errorType": "Function<StandardError>",
+                  "stackTrace": [
+                    "/var/task/pre_flight_checks.rb:145:in `handler"
+                  ]
+                }'
+            )
+          )
+        ]
+      )
+    end
+
+    it 'ouptuts that it failed' do
+      $stdout = StringIO.new
+
+      # Execute the script
+      load "#{__dir__}/../app.rb"
+
+      # Assert the output
+      expect($stdout.string).to eq("Deployment Failure Status: FAILED ❌\n")
+      expect(File.readlines(test_github_log.path)).to eq(
+        [
+          "deployment_failed=true\n",
+          'deployment_failure_reason=' \
+            'Forward deploy check FAILED. No need to panic! '\
+            'This likely means your commit has already been deployed as part of a previous deploy. '\
+            'To confirm you can check whether your SHA is a parent commit to the currently deployed SHA. '\
+            "You can figure out the currently deployed SHA by following this guide https://www.notion.so/freeagent/Deployment-Runbooks-29796221387e40b7abbb217d7d33c4ac?pvs=4#3bfa2ab5d3ab4c33b7a46522027f94bb\n"
+        ]
+      )
+    end
+  end
+
+  context 'when sidekiq failed to start' do
+    let(:desc_exec_resp) { Aws::States::Types::DescribeExecutionOutput.new(status: 'FAILED') }
+    let(:get_exec_history) do
+      Aws::States::Types::GetExecutionHistoryOutput.new(
+        events: [
+          '',
+          Aws::States::Types::HistoryEvent.new(
+            type: 'FailStateEntered',
+            state_entered_event_details: Aws::States::Types::StateEnteredEventDetails.new(
+              input: '
+                {
+                  "Error": {
+                    "Cause":"{\"errorMessage\":\"ECS deployment status: IN_PROGRESS\",\"errorType\":\"Function<DeployInProgress>\",\"stackTrace\":[\"/var/task/ecs_deployment_handler.rb:49:in `handler\"]}",
+                    "error":"Function<DeployInProgress>","resource":"invoke","resourceType":"lambda}"
+                  }
+                }
+              '
+            )
+          )
+        ]
+      )
+    end
+
+    it 'ouptuts that it failed' do
+      $stdout = StringIO.new
+
+      # Execute the script
+      load "#{__dir__}/../app.rb"
+
+      # Assert the output
+      expect($stdout.string).to eq("Deployment Failure Status: FAILED ❌\n")
+      expect(File.readlines(test_github_log.path)).to eq(
+        [
+          "deployment_failed=true\n",
+          "deployment_failure_reason=Sidekiq workers failed to start or failed to stabilise.\n"
+        ]
+      )
+    end
+  end
+end
